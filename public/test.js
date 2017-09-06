838dbb58fe6c6072a580f04589fd0c7a5bc0fb5a
Cody O'Ferrall <codyoferrall@Codys-MacBook-Air.local>
2017-09-06 14:24:26 -0700

    first commit

---
+++ public/test.js
@@ -0,0 +1,297 @@
+var socket;
+//var dropzone;
+
+var canv;
+
+var hist = [];
+var redo = [];
+
+var mic;
+
+var bground;
+
+var drawfunctions= {
+    stroker: true,
+    liner: false,
+    circle: false,
+    imager: false
+};
+
+
+
+function setup() {
+    canv = createCanvas(window.innerWidth, window.innerHeight);
+    canv.parent('canvas-holder');
+
+    bground = '#013220';
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('start', newStroke);
+    socket.on('mouse', stroking);
+    //socket.on('sound', newSound);
+    socket.on('undo', undoer);
+    socket.on('redo', redoer);
+    socket.on('clear', onclear);
+    
+    //dropzone = select('#dropzone');
+    //canv.dragOver(highlight);
+    //canv.dragLeave(unhighlight);
+    canv.drop(gotFile);
+
+    //mic = new p5.AudioIn();
+    //mic.start();
+    //console.log(typeof(mic));
+}
+
+function gotFile(file){
+
+    //drawImage(bground, 50, 50)
+    //background(bground);
+    //console.log(bground);
+    //img.hide();
+    //hist.push(new Imager(img, mouseX, mouseY));
+}
+
+
+//function highlight() {
+//    dropzone.style('background-color', '#ccc');
+//}
+
+//function unhighlight() {
+//    dropzone.style('background-color', '#2f2f2f');
+//}
+
+//function newSound(mic) {
+ //   sound = loadSound(mic);
+//    sound.play();
+//}
+
+function onclear() {
+    hist = [];
+    redo = [];
+}
+
+function newStroke(data) {
+    var position = createVector(data.x, data.y);
+    if (data.func == "stroker") {
+        hist.push(new Stroke(position));
+    }
+    
+    if (data.func == "liner") {
+        hist.push(new Line(position));
+    }
+    
+    if (data.func == "circle") {
+        hist.push(new Circle(position));
+    }
+}
+
+function stroking(data) {
+    // this might get buggy if multiple clients are drawing at 
+    // the same time. Might fix it by being more explicit
+    // about strokes index
+    var position = createVector(data.x, data.y);
+    hist[(hist.length - 1)].hist.push(position);    
+}
+
+function draw() {
+    cursor(CROSS);
+    background(bground);
+
+
+    stroke('#f5f5f5');
+
+    for (var i = 0; i < hist.length; i++) {
+        hist[i].display();
+    }
+   // var vol = mic.getLevel();
+    //console.log(vol);
+    
+    //socket.emit("sound", mic);
+}
+
+
+function mousePressed() {
+    var position = createVector(mouseX, mouseY);
+    if(position.y >50) {
+    if (drawfunctions.stroker) {
+        hist.push(new Stroke(position));
+        console.log("new stroke!");
+        var data = {
+        func: "stroker",
+        x: mouseX,
+        y: mouseY
+    };
+    }
+    else if (drawfunctions.liner) {
+        hist.push(new Line(position));
+        console.log("new line!");
+        var data = {
+        func: "liner",
+        x: mouseX,
+        y: mouseY
+    };
+    }
+    else if (drawfunctions.circle) {
+        hist.push(new Circle(position));
+        console.log("new circle!");
+        var data = {
+            func: "circle",
+            x: mouseX,
+            y: mouseY
+        };
+    }
+    else if (drawfunctions.imager) {
+        hist.push(new Imager(position));
+        console.log("new image!");
+        var data = {
+            func: "imager",
+            x: mouseX,
+            y: mouseY
+        };
+    }
+    else {
+        console.log("no draw function selected");
+    }
+}
+ 
+    socket.emit('start', data);
+}
+
+function mouseDragged() {
+    var position = createVector(mouseX, mouseY);
+    if(position.y >50) {
+    hist[(hist.length - 1)].hist.push(position);
+    //console.log(hist[0].hist[0]);
+    var data = {
+        x: mouseX,
+        y: mouseY
+    };
+        
+    socket.emit('mouse', data);
+}
+}
+
+function Grid(vector) {
+    this.hist = [vector, vector];
+    
+   // this.display = function() {
+     //   for (var = i; i <)
+    //}
+}
+
+function Circle(vector) {
+    this.hist = [vector, vector];
+    this.display = function() {
+        rad = sqrt(sq((this.hist[0].x - this.hist[(this.hist.length - 1)].x)) + sq((this.hist[0].y - this.hist[(this.hist.length - 1)].y)));
+         noFill();
+         beginShape();
+         ellipse(this.hist[0].x, this.hist[0].y, rad * 2);
+         endShape();
+    }
+}
+
+function Line(vector) {
+    this.hist = [vector, vector];
+    //this.hist = [vector, this.hist[(hist.length - 1)]];
+    //this.hist.push(vector);
+    
+    
+    //var position = createVector(mouseX, mouseY);
+    //this.history[1] = postion;
+    this.display = function() {
+        beginShape();
+        vertex(this.hist[0].x, this.hist[0].y);
+        vertex(this.hist[(this.hist.length - 1)].x, this.hist[(this.hist.length - 1)].y);
+        endShape();
+    }
+
+}
+
+
+
+function Stroke(vector) {
+    this.hist = [vector, vector];
+    //this.hist.push(vector);
+    
+    this.display = function() {
+        noFill();
+        smooth();
+        beginShape();
+        for (var i = 0; i < this.hist.length; i++) {
+            var pos = this.hist[i];
+            vertex(pos.x, pos.y);
+        }
+        endShape();
+    }   
+}
+
+function Imager(img, x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.display = function() {
+        image(img, 50, 50, 200, 200);
+    }   
+}
+
+function stroke_selector() {
+        drawfunctions.stroker = true;
+        drawfunctions.liner = false;
+        drawfunctions.circle = false;
+}
+    
+ function line_selector() {
+        drawfunctions.stroker = false;
+        drawfunctions.liner = true;
+        drawfunctions.circle = false;
+}
+
+ function circle_selector() {
+        drawfunctions.stroker = false;
+        drawfunctions.liner = false;
+        drawfunctions.circle = true;
+}
+
+function undoSend(){
+   socket.emit("undo");
+}
+
+function undoer() {
+ 
+    redo.push(hist[(hist.length - 1)]);
+    console.log(hist.length);
+    hist.pop();
+    console.log(hist.length);
+}
+
+function redoSend() {
+    socket.emit("redo");	
+}
+
+function redoer() {
+    if(redo.length > 0) {
+        hist.push(redo[(redo.length - 1)]);
+        redo.pop();
+    }
+}
+
+function fit() {
+    canv = createCanvas(window.innerWidth, window.innerHeight);
+    //canv.parent('canvas-holder');
+
+    background(bground);
+}
+
+function clearPage() {
+    hist = [];
+    redo = [];
+    console.log(hist.length);
+    socket.emit("clear");
+}
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,85 @@
+// express is a server program for running a local server
+var express = require('express');
+
+// this sets up our server on port 3000
+var app = express();
+var server = app.listen(3000);
+
+//this just shows the contents of our 'public' directory
+// that's where index.html and sketch.js are at
+app.use(express.static('public'));
+
+
+console.log("My socket server is running");
+
+// imports the socket library and sets it up as a function
+var socket = require('socket.io');
+
+// calls the socket function with our server as its argument
+// sets it up as a variable 'io' so that we can do things with it
+var io = socket(server);
+
+// checking for a new connection event
+io.sockets.on('connection', newConnection);
+
+// new connection function
+function newConnection(socket){
+    //logs the new connection's id
+    console.log('new connection: ' + socket.id);
+
+    // checks for a message coming from the particular
+    // connection that we just made
+    // "if we recieve a msg named 'mouse',
+    // trigger the function 'mouseMsg' "
+    socket.on('start', startMsg);
+    socket.on('mouse', mouseMsg);
+    socket.on('sound', soundMsg);
+    socket.on('undo', undoMsg);
+    socket.on('redo', redoMsg);
+    socket.on('clear', clearMsg);
+
+    function clearMsg() {
+        socket.broadcast.emit('clear');
+    }
+    
+    function undoMsg() {
+        socket.broadcast.emit('undo');
+    }
+
+   function redoMsg() {
+        socket.broadcast.emit('redo');
+    }
+
+    function soundMsg(data) {
+        socket.broadcast.emit('sound', data);
+    }
+    
+    function startMsg(data) {
+        
+        //sends stroke start data to other clients
+        socket.broadcast.emit('start', data);
+        
+        //sends data to all clients, original sender included
+        //io.sockets.emit('mouse', data);
+        
+        //console.log(data);
+    }
+    
+    //  logs the msg 'mouse'
+    function mouseMsg(data) {
+        
+        //sends data to other clients
+        socket.broadcast.emit('mouse', data);
+        
+        //sends data to all clients, original sender included
+        //io.sockets.emit('mouse', data);
+        
+        //console.log(data);
+    }
+
+}
+
+
+
+
+
